1eecede3fdc0cc9474a4d0e7ff96c350bbb9941b
Kevin Dietrich <hallo@kevin-dietrich.com>
2018-07-25 08:56:49 +0200

    SimpleCMDPlugin: pauseCheckIntervalAfterSwitch

--- dist/plugins/SimpleCMDPlugin.js
+++ dist/plugins/SimpleCMDPlugin.js
@@ -8,6 +8,7 @@ var SimpleCMDPlugin = /** @class */ (function () {
     function SimpleCMDPlugin() {
         this.name = 'SimpleCMDPlugin';
         this.devices = [];
+        this.checkIntervalPaused = false;
     }
     SimpleCMDPlugin.prototype.init = function (p) {
         logger.debug('init(%s)', JSON.stringify(p));
@@ -15,13 +16,17 @@ var SimpleCMDPlugin = /** @class */ (function () {
         this.offCmd = p.pluginParams.offCmd;
         this.statusCmd = p.pluginParams.statusCmd;
         this.checkInterval = p.pluginParams.checkInterval;
+        this.pauseCheckIntervalAfterSwitch = p.pluginParams.pauseCheckIntervalAfterSwitch;
         var device = new HMLCSW1_1.HMLCSW1(p.deviceName);
         device.events.on('onTurnOn', this.onTurnOn.bind(this));
         device.events.on('onTurnOff', this.onTurnOff.bind(this));
         this.devices.push(device);
         var that = this;
         setInterval(function () {
-            that._checkStatus(device, false);
+            if (!that.checkIntervalPaused)
+                that._checkStatus(device, false);
+            else
+                logger.info('Checking status is paused.', that.name);
         }, that.checkInterval);
         that._checkStatus(device, true);
         logger.info('Plugin %s initialized.', this.name);
@@ -31,6 +36,8 @@ var SimpleCMDPlugin = /** @class */ (function () {
         logger.debug('onTurnOn()');
         logger.info('Device %s turned on.', device.deviceName);
         logger.info('Executing onCmd.');
+        var that = this;
+        that._pauseCheckStatus();
         exec(this.onCmd, function (err, stdout, stderr) {
             if (err) {
                 logger.error('onCmd failed with error %s', err);
@@ -43,6 +50,8 @@ var SimpleCMDPlugin = /** @class */ (function () {
         logger.debug('onTurnOff()');
         logger.info('Device %s turned off.', device.deviceName);
         logger.info('Executing offCmd.');
+        var that = this;
+        that._pauseCheckStatus();
         exec(this.offCmd, function (err, stdout, stderr) {
             if (err) {
                 logger.error('offCmd failed with error %s', err);
@@ -56,6 +65,8 @@ var SimpleCMDPlugin = /** @class */ (function () {
         var that = this;
         logger.info('Checking status of %s.', device.deviceName);
         exec(that.statusCmd, function (err, stdout, stderr) {
+            if (that.checkIntervalPaused)
+                return;
             if (err) {
                 logger.error('statusCmd failed with error %s', err);
                 return;
@@ -63,12 +74,23 @@ var SimpleCMDPlugin = /** @class */ (function () {
             var val = stdout === '0\n';
             if (val !== device.state1 || forceRefresh) {
                 logger.info('Status of %s changed to %s.', device.deviceName, val);
+                if (!forceRefresh)
+                    that._pauseCheckStatus();
                 device.stateChanged(1, val);
             }
             else {
                 logger.info('Status of %s has not changed.', device.deviceName);
             }
         });
+    };
+    SimpleCMDPlugin.prototype._pauseCheckStatus = function () {
+        var that = this;
+        if (!that.pauseCheckIntervalAfterSwitch || that.checkIntervalPaused)
+            return;
+        that.checkIntervalPaused = true;
+        setTimeout(function () {
+            that.checkIntervalPaused = false;
+        }, that.pauseCheckIntervalAfterSwitch);
     };
     return SimpleCMDPlugin;
 }());
--- src/plugins/SimpleCMDPlugin.ts
+++ src/plugins/SimpleCMDPlugin.ts
@@ -12,6 +12,8 @@ export class SimpleCMDPlugin implements PluginInterface {
     offCmd: String;
     statusCmd: String;
     checkInterval: number;
+    pauseCheckIntervalAfterSwitch : number;
+    checkIntervalPaused: boolean = false;
 
     init(p) {
         logger.debug('init(%s)', JSON.stringify(p));
@@ -19,6 +21,7 @@ export class SimpleCMDPlugin implements PluginInterface {
         this.offCmd = p.pluginParams.offCmd;
         this.statusCmd = p.pluginParams.statusCmd;
         this.checkInterval = p.pluginParams.checkInterval;
+        this.pauseCheckIntervalAfterSwitch = p.pluginParams.pauseCheckIntervalAfterSwitch;
 
         let device = new HMLCSW1(p.deviceName);
         device.events.on('onTurnOn', this.onTurnOn.bind(this));
@@ -27,7 +30,10 @@ export class SimpleCMDPlugin implements PluginInterface {
 
         var that = this;
         setInterval(function() {
-            that._checkStatus(device, false);
+            if(!that.checkIntervalPaused)
+                that._checkStatus(device, false);
+            else
+                logger.info('Checking status is paused.', that.name);
         }, that.checkInterval);
         that._checkStatus(device, true);
 
@@ -40,6 +46,8 @@ export class SimpleCMDPlugin implements PluginInterface {
         logger.debug('onTurnOn()');
         logger.info('Device %s turned on.', device.deviceName);
         logger.info('Executing onCmd.');
+        let that = this;
+        that._pauseCheckStatus();
         exec(this.onCmd, function(err, stdout, stderr) {
             if(err) {
                 logger.error('onCmd failed with error %s', err);
@@ -53,6 +61,8 @@ export class SimpleCMDPlugin implements PluginInterface {
         logger.debug('onTurnOff()');
         logger.info('Device %s turned off.', device.deviceName);
         logger.info('Executing offCmd.');
+        let that = this;
+        that._pauseCheckStatus();
         exec(this.offCmd, function(err, stdout, stderr) {
             if(err) {
                 logger.error('offCmd failed with error %s', err);
@@ -67,6 +77,8 @@ export class SimpleCMDPlugin implements PluginInterface {
         var that = this;
         logger.info('Checking status of %s.', device.deviceName);
         exec(that.statusCmd, function(err, stdout, stderr) {
+            if(that.checkIntervalPaused)
+                return;
             if(err) {
                 logger.error('statusCmd failed with error %s', err);
                 return;
@@ -74,12 +86,24 @@ export class SimpleCMDPlugin implements PluginInterface {
             let val = stdout==='0\n';
             if(val !== device.state1 || forceRefresh) {
                 logger.info('Status of %s changed to %s.', device.deviceName, val);
+                if(!forceRefresh)
+                    that._pauseCheckStatus();
                 device.stateChanged(1, val);
             }
             else {
                 logger.info('Status of %s has not changed.', device.deviceName);
             }
         });
+    }
+
+    _pauseCheckStatus() {
+        let that = this;
+        if(!that.pauseCheckIntervalAfterSwitch || that.checkIntervalPaused)
+            return;
+        that.checkIntervalPaused = true;
+        setTimeout(function() {
+            that.checkIntervalPaused = false;
+        }, that.pauseCheckIntervalAfterSwitch);
     }
 
 }
